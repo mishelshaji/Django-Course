6852056489cf3402083929194761d8ef59f427a1
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2021-05-28 12:36:33 +0530

    if-else

--- Project 1 - Blog/blog/user/views.py
+++ Project 1 - Blog/blog/user/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    context = {'data': ['PHP']}
-    return render(request, 'user/home.html')
+    context = {'data': True, 'age': 25}
+    return render(request, 'user/home.html', context)
 
 def about(request):
     return render(request, 'user/about.html')
