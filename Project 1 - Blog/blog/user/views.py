d70e1d117b9e28f8473bf3147561fd78b890c613
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2021-05-28 12:22:03 +0530

    for-empty

--- Project 1 - Blog/blog/user/views.py
+++ Project 1 - Blog/blog/user/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    context = {'data': ['Python', 'PHP', 'Java']}
-    return render(request, 'user/home.html', context)
+    context = {'data': ['PHP']}
+    return render(request, 'user/home.html')
 
 def about(request):
     return render(request, 'user/about.html')
