4f85a8fbdcc9a7060e8893cc8efe3da486a82e05
Mishel Shaji <33393350+mishelshaji@users.noreply.github.com>
2021-06-14 12:33:39 +0530

    Category List And Update

--- Project 1 - Blog/blog/administrator/urls.py
+++ Project 1 - Blog/blog/administrator/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('category/create/', create_category, name="admin_create_category")
+    path('category/', list_category, name="admin_list_category"),
+    path('category/create/', create_category, name="admin_create_category"),
+    path('category/update/<int:id>/', update_category, name="admin_update_category"),
 ]
--- Project 1 - Blog/blog/administrator/views.py
+++ Project 1 - Blog/blog/administrator/views.py
@@ -18,3 +18,25 @@ def create_category(request):
             context = {}
             context['form'] = form
             return render(request, 'administrator/category/create.html', context)
+
+def list_category(request):
+    categories = Category.objects.all()
+    context = {}
+    context['data'] = categories
+    return render(request, 'administrator/category/list.html', context)
+
+def update_category(request, id):
+    category = Category.objects.get(id=id)
+    if request.method == 'GET':
+        context = {}
+        context['form'] = CategoryForm(instance=category)
+        return render(request, 'administrator/category/update.html', context)
+    else:
+        form = CategoryForm(data=request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Data Saved")
+        else:
+            context = {}
+            context['form'] = form
+            return render(request, 'administrator/category/update.html', context)
